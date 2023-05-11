eb9e8e5007e5f1328b23a0c486352facba143a8d
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-05-11 22:48:41 +0700

    first commit in repo

---
+++ script.js
@@ -0,0 +1,8 @@
+function dropdown() {
+    var x = document.getElementById("list");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
